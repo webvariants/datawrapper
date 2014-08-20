e974b7ab1278847f94e3e95d3681d1bf6d813cdf
Christoph <christoph@webvariants.de>
2014-08-20 21:44:24 +0200

    more obligatory license comments

--- plugins/visualization/plugin.php
+++ plugins/visualization/plugin.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Copyright (c) 2014, Der Akademie Berufliche Bildung der deutschen Zeitungsverlage e. V.
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
 
 use Datawrapper\Plugin;
 use Datawrapper\Visualization;
