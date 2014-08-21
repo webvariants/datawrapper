0655b1a95c1918dde07847e5ffc19d0bce9e7913
Christoph <christoph@webvariants.de>
2014-08-22 00:21:19 +0200

    updated analytics plugin

--- plugins/analytics-piwik/plugin.php
+++ plugins/analytics-piwik/plugin.php
@@ -19,29 +19,32 @@ class DatawrapperPlugin_AnalyticsPiwik extends Plugin {
         Hooks::register(Hooks::CORE_AFTER_BODY,  array($this, 'getTrackingCode'));
     }
 
-    public function getTrackingCode(Chart $chart) {
+    public function getTrackingCode(Chart $chart = null) {
         $config = $this->getConfig();
         if (empty($config)) return false;
 
         $url    = $config['url'];
         $idSite = $config['idSite'];
-        $user   = $chart->getUser();
 
         print '<!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
   _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-  _paq.push(["setCookieDomain", "*.www.datawrapper.de"]);
-  '.(is_a($chart, 'Chart') && $user ?
- '_paq.push(["setCustomVariable", 1, "Layout", "'.$chart->getTheme().'", "page"]);
-  _paq.push(["setCustomVariable", 2, "Author", "'.$user->getId().'", "page"]);
-  _paq.push(["setCustomVariable", 3, "Visualization", "'.$chart->getType().'", "page"]);
-  ' : '').'
+  _paq.push(["setCookieDomain", "*.www.datawrapper.de"]);';
+
+        if ($chart && $chart->getUser()) {
+            print '
+  _paq.push(["setCustomVariable", 1, "Layout", "'.$chart->getTheme().'", "page"]);
+  _paq.push(["setCustomVariable", 2, "Author", "'.$chart->getUser()->getId().'", "page"]);
+  _paq.push(["setCustomVariable", 3, "Visualization", "'.$chart->getType().'", "page"]);';
+        }
+
+        print '
   _paq.push(["trackPageView"]);
   _paq.push(["enableLinkTracking"]);
 
   (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://' . $url . '/";
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://'.$url.'/";
     _paq.push(["setTrackerUrl", u+"piwik.php"]);
     _paq.push(["setSiteId", "1"]);
     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
